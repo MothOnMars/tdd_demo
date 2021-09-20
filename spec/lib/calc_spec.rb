0a22b1176761858c899ce28f6ae8fae6b86e7f3d
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2021-09-20 15:50:02 -0400

    add spec for non-nil value

--- spec/lib/calc_spec.rb
+++ spec/lib/calc_spec.rb
@@ -3,5 +3,9 @@ describe Calc do
     it 'has an "add" method' do
       expect(Calc).to respond_to :add
     end
+
+    it 'does something' do
+      expect(Calc.add(4,0)).not_to be nil
+    end
   end
 end
