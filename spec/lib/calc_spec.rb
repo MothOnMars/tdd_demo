ce6b725517dd6d07dd07d6f21d10f40848073be7
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2021-09-20 16:19:38 -0400

    too many specs

--- spec/lib/calc_spec.rb
+++ spec/lib/calc_spec.rb
@@ -9,8 +9,14 @@ describe Calc do
     end
 
     it 'returns the sum of the numbers' do
-      expect(Calc.add(4,0)).to eq 4
-      expect(Calc.add(4,1)).to eq 5
+      expect(Calc.add(4,1)).to eq 5 # This is the only test needed for dice!
+      expect(Calc.add(4, -1)).to eq 3
+      expect(Calc.add(4, 0.5)).to eq 4.5
+      expect(Calc.add(4, 1_000_000_000)).to eq 1_000_000_004
+      expect(Calc.add('4', '1')).to eq 5
+      expect(Calc.add('four', 'one')).to eq 5
+      expect(Calc.add(4, π)).to eq 7.14159 #.....
+      expect(Calc.add(1,2,3)).to eq 6
     end
   end
 end
