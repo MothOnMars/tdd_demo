cd7fcd1bccc02827c0c1d84b6ccfd7b554787b1a
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2021-09-22 09:20:06 -0400

    problemz

--- app/controllers/calc.rb
+++ app/controllers/calc.rb
@@ -1,5 +1,24 @@
+require 'open-uri'
+require 'benchmark'
+require 'csv'
+
 module Calc
+
+
   def self.add(a, b)
-    Date.today.friday? ? 5 : (a + b)
+    array = [a, b]
+  array.map(&:to_s).collect do |number|
+      hip_hop_homies = number
+      hip_hop_homies.to_i
+    end.sum
+  end
+
+  # this method multiplies numbers
+  def self.subtrakt(a ,b)
+     a - b
+  end
+
+  def self.fat_bear_info
+    open('https://www.nps.gov/katm/learn/fat-bear-week.htm').read
   end
 end
--- spec/controllers/calc_spec.rb
+++ spec/controllers/calc_spec.rb
@@ -13,4 +13,17 @@ describe Calc do
       expect(Calc.add(4, 1)).to eq 5
     end
   end
+
+  describe '.subtrakt' do
+    it 'returns the sum of the numbers' do
+      expect(Calc.subtrakt(4, 0)).to eq 4
+    end
+  end
+
+  describe '.fat_bear_info' do
+    it 'returns information about Fat Bear Week' do
+      expect(Calc.fat_bear_info).to match /2021 Fat Bear Week/
+    end
+
+  end
 end
